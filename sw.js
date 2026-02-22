bc0165a97f85c6811b786e900607cfa5c7936937
yashpasale253-lang <yashpasale253@gmail.com>
2026-02-22 14:49:53 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,3 @@
-const CACHE_NAME = 'location-tracker-v1';
-const urlsToCache = [
-    '/',
-    '/index.html'
-];
-
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
-});
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-    );
-});
+const CACHE = 'gps-tracker-v2';
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/', '/index.html']))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
